909a7e7225374f96348eb85ba0a566a24e82aa55
WeiSheng Su <wilsonsu@yahoo-inc.com>
2015-02-07 13:29:50 -0800

    Initial Commit

---
+++ Twitter/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Twitter
+//
+//  Created by WeiSheng Su on 2/7/15.
+//  Copyright (c) 2015 Wilson Su. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
